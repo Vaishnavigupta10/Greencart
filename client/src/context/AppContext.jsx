6645dda3bc294680f68a216ebce91f961b6709b5
Vaishnavi Gupta <148614725+Vaishnavigupta10@users.noreply.github.com>
2025-07-01 03:45:56 +0530

    day1

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,77 @@
+import { createContext , useState,useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+
+export const AppContext = createContext();
+
+
+export const AppContextProvider = ({ children }) => {
+
+  const currency=import.meta.VITE_CURRENCY;
+
+   const navigate=useNavigate();
+   const [user,setUser]=useState(null);
+   const [isSeller,setIsSeller]=useState(false);
+   const [showUserLogin,setShowUserLogin]=useState(false);
+  const [products,setProducts]=useState([])
+const [cartItems,setCartItems]=useState([]);
+
+  const fetchProducts=async()=>{
+    setProducts(dummyProducts)
+  }
+
+
+
+
+   const addToCart=(itemId)=>{
+  let cartData=structuredClone(cartItems); 
+  if(cartData[itemId]){
+    cartData[itemId]+=1;
+  }else{
+    cartData[itemId]=1;
+  }
+  setCartItems(cartData);
+  toast.success("Item added to cart");
+  }
+
+// Update Cart Item Quantity
+const updateCartItem = (itemId, quantity) => {
+  let cartData = structuredClone(cartItems); // Deep clone of current cart
+  cartData[itemId] = quantity;              // Update quantity
+  setCartItems(cartData);                   // Set new cart state
+  toast.success("Updated cart");            // Toast notification
+};
+
+// Remove Product from Cart
+const removeFromCart = (itemId) => {
+  let cartData = structuredClone(cartItems);
+  if (cartData[itemId]) {
+    cartData[itemId] -= 1;
+    if (cartData[itemId] === 0) {
+      delete cartData[itemId];
+    }
+  }
+  toast.success("Removed from Cart");
+  setCartItems(cartData);
+};
+
+
+
+
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+
+
+  const value={navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products,currency,addToCart,updateCartItem,removeFromCart,cartItems};
+
+
+  return <AppContext.Provider value={value}>
+    {children}
+  </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+}
